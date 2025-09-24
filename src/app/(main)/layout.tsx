c7a768a791b54ac3a296a09704a1e9f1ff2e8eca
unknown <huzaifa.tanveer@launchbox.pk>
2025-09-24 14:39:18 +0500

    animated loader added

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,20 +1,38 @@
+"use client"; // since we’re using state + useEffect
 
+import { useEffect, useState } from "react";
 import ChatWidget from "@/components/chat-widget";
 import ScrollToTopButton from "@/components/scroll-to-top-btn";
 import Footer from "@/components/ui/footer";
 import Navbar from "@/components/ui/navbar";
+import Image from "next/image";
 
-export default function layout({
+export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 5000); // 3 seconds
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen w-screen bg-white">
+        <Image src="/images/loader.gif" alt="Loading..." width={500} height={500} className="" />
+      </div>
+    );
+  }
+
   return (
     <div>
       <Navbar />
       {children}
       <ChatWidget />
-      <ScrollToTopButton/>
+      <ScrollToTopButton />
       <Footer />
     </div>
   );
